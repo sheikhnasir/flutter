073360839e6226cd2f3b5038833e8366e207dab2
Sheikh Nasir <sheikhnasir@virtualspaces.co>
2025-08-07 00:24:15 +0800

    Project 4

---
+++ ProjectDetails/4/chat_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/demo/counter_stateful_demo.dart';
+import 'package:chat_app/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Chat App",
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: const CounterStateful(),
+    );
+  }
+}
